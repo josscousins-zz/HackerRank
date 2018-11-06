1fd29108a30c5c6fd47d886ad8cfafba322748ae
josscousins <josscousins@yahoo.com>
2018-11-06 16:15:11 +0000

    my solution to A Very Big Sum

---
+++ Practice/Algorithms/Warmup/AVeryBigSum/src/Solution.java
@@ -0,0 +1,57 @@
+/**
+ * @author CousinsJ
+ *	Desc: sum the contents of an Array of type = long
+ */
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // calculate the sum of an Array
+    static long aVeryBigSum(long[] ar) {
+
+        long bigNum =0;
+
+        //iterate through ar
+        for(long i: ar) {
+        	bigNum += i;
+            
+        }
+        
+        return bigNum;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out) );
+
+        int arCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        long[] ar = new long[arCount];
+
+        String[] arItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < arCount; i++) {
+            long arItem = Long.parseLong(arItems[i]);
+            ar[i] = arItem;
+        }
+
+        long result = aVeryBigSum(ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
+
